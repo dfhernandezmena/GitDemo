24356e91fdda302bf996faf1befe7bdeaaf2aa9f
Douglas F. Hernandez <dfhernandez@gmail.com>
2021-02-25 14:14:51 -0400

    Actualización de la sección Head de la página

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <html>
 <head>
+<meta charset="utf-8">
+<title>Página de GIT</title>
 </head>
 <body>
   <h2><?php echo 'Hola Douglano'; ?></h2>
+  <p>El aprendizaje de nuevas herramientas de trabajo tienen su precio<p>
 </body>
-
 </html>
